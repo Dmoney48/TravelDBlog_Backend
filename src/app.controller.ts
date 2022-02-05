fb42818913a474c2fc88e99072af2a5b2dc0de2c
Drew Hyatt <drew.hyatt@getbuilt.com>
2022-02-04 23:13:46 -0600

    Nest.JS Boilerplate

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// import Parser from 'rss-parser';
+// type CustomFeed = { foo: string };
+// type CustomItem = { bar: number };
+// const parser: Parser<CustomFeed, CustomItem> = new Parser({
+//     customFields: {
+//         feed: ['foo'],
+//         item: ['bar']
+//     }
+// });
+
+// (async () => {
+
+//     const feed = await parser.parseURL('https://www.reddit.com/.rss');
+//     console.log(feed.title); // feed will have a `foo` property, type as a string
+
+//     feed.items.forEach(item => {
+//         console.log(item.title + ':' + item.link) // item will have a `bar` property type as a number
+//     });
+// })();
+
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) { }
