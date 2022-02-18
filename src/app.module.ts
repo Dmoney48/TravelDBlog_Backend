7b7ad8914e211497ee0818235baa5bb6ac606c77
Drew Hyatt <drew.hyatt@getbuilt.com>
2022-02-17 23:15:53 -0600

    Updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
 import { UserModule } from './user/user.module';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
     imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'client'),
+        }),
         ConfigModule.forRoot({
             envFilePath: ['.env'],
             load: [configuration],
